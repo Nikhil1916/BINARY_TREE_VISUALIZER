6a2c7bf20363300054d22d94af3357159a9f58a6
nikhil <nikhilchawla9013@gmail.com>
2024-02-26 00:17:33 +0530

    first commit added binary node class

---
+++ binaryTreeNode.js
@@ -0,0 +1,21 @@
+class Node {
+    lc = rc = value = null;
+    constructor(value) {
+        this.value = value;
+        this.lc = this.rc = null;
+    }
+
+    setLeftChild(node) {
+        this.lc = node;
+    }
+
+    setRightChild(node) {
+        this.rc = node;
+    }
+
+    getHeight() {
+        const leftHeight = this.lc?.getHeight() || 0;
+        const rightHeight = this.lc?.getHeight() || 0;
+        return Math.max(leftHeight , rightHeight) + 1;
+    }
+}
