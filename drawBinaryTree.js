63487cf29834468c5b2b4b69416bae1069264ae2
nikhil <nikhilchawla9013@gmail.com>
2024-02-26 23:25:41 +0530

    create node and connect nodes

--- drawBinaryTree.js
+++ drawBinaryTree.js
@@ -1,5 +1,5 @@
 import { Node } from "./binaryTreeNode.js";
-import { getRequiredHeightAndWidth } from "./treeUtils.js";
+import { getRequiredHeightAndWidth , DEFAULT_CONFIG , drawNode , connectEdges } from "./treeUtils.js";
 
 const canvas = document.querySelector("canvas");
 
@@ -9,6 +9,7 @@ function drawBinaryTree(root, canvasElement) {
     const maxWidth = window.innerWidth;
 
     // initialize canvas dimesions
+    console.log(maxHeight , maxWidth);
     canvasElement.width = maxWidth;
     canvasElement.height = maxHeight;
 
@@ -16,8 +17,10 @@ function drawBinaryTree(root, canvasElement) {
     // calculate require width and height to draw tree structure jo box hei jisme humara tree hoga
     const { reqCanvasHeight, reqCanvasWidth } = getRequiredHeightAndWidth(root);
 
+    // console.log(reqCanvasHeight , reqCanvasWidth);
+
     // as we want to place the tree at window center so finding starting and ending point in x-axis
-    const windowCenter = maxHeight / 2;
+    const windowCenter = maxWidth / 2;
     const requiredWidthCenter = reqCanvasWidth / 2;
 
     const xStart = windowCenter - requiredWidthCenter;
@@ -28,6 +31,63 @@ function drawBinaryTree(root, canvasElement) {
         xEnd
     };
 
+    recursivelyDrawNodes(root, canvasElement , 0.5 , horizontalConfig);
+}
+
+
+function recursivelyDrawNodes(root , canvasElement, currentLevel , horizontalConfig) {
+    const {xStart , xEnd} =  horizontalConfig;
+
+    const xPos = (xStart + xEnd)/2;
+    const yPos = currentLevel * DEFAULT_CONFIG.nodeHeightSpacing;
+
+    drawNode(root.value , canvasElement, xPos , yPos);
+    if(root.lc!==null) {
+        const leftNodeHorizontalConfig = {
+            xStart,
+            xEnd: xPos
+        }
+
+        recursivelyDrawNodes(root.lc , canvasElement, currentLevel+1 , leftNodeHorizontalConfig);
+        //connect edges
+        connectEdges(
+          canvasElement,
+          {
+            xStart: xPos,
+            xEnd: (xStart + xPos) / 2,
+          },
+          {
+            yStart: yPos + DEFAULT_CONFIG.radius,
+            yEnd:
+              ((currentLevel +
+              1) * DEFAULT_CONFIG.nodeHeightSpacing) -
+              DEFAULT_CONFIG.radius,
+          }
+        );
+    }
+
+    if(root.rc!==null) {
+        const rightNodeHorizontalConfig = {
+            xStart: xPos,
+            xEnd
+        }
+        recursivelyDrawNodes(root.rc , canvasElement, currentLevel+1 , rightNodeHorizontalConfig);
+        // /connect edges
+        connectEdges(
+            canvasElement,
+            {
+              xStart: xPos,
+              xEnd: (xPos + xEnd) / 2,
+            },
+            {
+              yStart: yPos + DEFAULT_CONFIG.radius,
+              yEnd:
+               ( (currentLevel +
+                1 )* DEFAULT_CONFIG.nodeHeightSpacing) -
+                DEFAULT_CONFIG.radius,
+            }
+          );
+    }
 }
 
 const root = new Node(10);
@@ -39,6 +99,10 @@ const node3 = new Node(3);
 root.setLeftChild(node2);
 
 root.setRightChild(node3);
+
+node2.setLeftChild(new Node(10));
+node3.setLeftChild(new Node(10));
+// node2.setLeftChild(new Node(10));
 
 console.log(root);
 
--- treeUtils.js
+++ treeUtils.js
@@ -1,14 +1,15 @@
 export const DEFAULT_CONFIG = {
     radius: 20,
-    nodeWidthSpacing: 25, // width widht and height are bigger then node ek box mei node hei suppose kro
-    nodeHeightSpacing: 90, // line height or Y-axis height
-    fontSize: 10
+    nodeWidthSpacing: 30, // width and height are bigger then node ek box mei node hei suppose kro
+    nodeHeightSpacing: 100, // line height or Y-axis height
+    fontSize: 10    
 }
 
 
 
 export function getRequiredHeightAndWidth(root) {
     const treeHeight = root.getHeight();
+    // console.log(treeHeight);
     // to get canvas height we will use tree Hieght which will be Node line height * height of tree
     const reqCanvasHeight = treeHeight * DEFAULT_CONFIG['nodeHeightSpacing'];
 
@@ -20,4 +21,50 @@ export function getRequiredHeightAndWidth(root) {
         reqCanvasHeight,
         reqCanvasWidth
     }
+}
+
+export function drawNode(value , canvasElement , x , y) {
+    const context = canvasElement.getContext("2d"); // tool to draw
+
+    // draw circle
+    context.beginPath();
+    context.arc(x, y, DEFAULT_CONFIG.radius , 0 , 2*Math.PI);
+    context.fillStyle = 'lightsalmon';
+    context.fill();
+
+
+    // /draw circle border
+    context.beginPath();
+    context.arc(x, y, DEFAULT_CONFIG.radius , 0 , 2*Math.PI);
+    context.strokeStyle = 'brown';
+    context.stroke();
+
+    // fill value
+    context.font = `${DEFAULT_CONFIG?.fontSize}pt serif`;
+    context.fillStyle = 'brown';
+    context.textAlign = 'center';
+    context.fillText(value , x , y + DEFAULT_CONFIG?.fontSize/2);
+
+}
+
+export function connectEdges(canvasElement , xCoordinates, yCoordinates) {
+    const {xStart , xEnd} = xCoordinates;
+    const {yStart , yEnd} = yCoordinates;
+
+    // draw curve
+    const start = {
+        x: xStart,
+        y: yStart
+    }
+
+    const end = {
+        x: xEnd,
+        y: yEnd
+    }
+    const context = canvasElement.getContext("2d"); // tool to draw
+    context.beginPath();
+    context.strokeStyle = 'brown';
+    context.moveTo(start.x , start.y);
+    context.lineTo(end.x, end.y);
+    context.stroke();
 }
